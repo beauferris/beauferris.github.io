6b3f8a38bc9b43276455b0ee64fad4d2c44b39dc
beauferris <h-beauferris@hotmail.com>
2022-02-16 13:20:20 -0700

    fixed colors

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,26 +2,26 @@ import './Project.css'
 import { Link } from 'react-router-dom'
 const Project = (props) => {
     return (
-        <Link to={`/projects/${props.name}` }>
-       
+
+
         <div className='card'>
+            <Link to={`/projects/${props.name}`}>
+                <div className='title-card'>
+                    <p className='card-type' >WEB APP</p>
+                    <p className='card-name'> {props.name}</p>
+                </div>
 
-            <div className='title-card'>
-                <p className='card-type' >WEB APP</p>
-                <p className='card-name'> {props.name}</p>
-            </div>
-            
-            <img className='thumbnail' src={props.img} alt=''></img>
+                <img className='thumbnail' src={props.img} alt='' />
 
-            {/* <a href={props.git} rel="noreferrer" target="_blank" className='git-button'>
+                {/* <a href={props.git} rel="noreferrer" target="_blank" className='git-button'>
                 <img src='./images/github.svg' alt="github icon"></img>
             </a>
             <a href={props.url} rel="noreferrer" target="_blank" className='view-button'>
                 <img src='./images/arrow.svg' alt="github icon"></img>
             </a> */}
-
+            </Link>
         </div>
-        </Link>
+
     )
 }
 
